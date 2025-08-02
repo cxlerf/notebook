d9380b513f24df7a9143a0781f142ecdbaddb480
cxl <cxl57432@gmail.com>
2025-08-03 04:39:40 +0800

    更新

--- test2.cpp
+++ test2.cpp
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main(){
-    int i = 1;
-    for (int j = 0; j < 5; j++) {
-        printf("%d\n",i);
-        i++;
-    }
+    int a =12;
+    a+=a-=a*a;
+    printf("%d\n",a);
 }
